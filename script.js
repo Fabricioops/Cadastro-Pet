37be367af7f3128033f5cfde0dcbaba1abb130bc
Fabricio <fabricioztxsantana@gmail.com>
2025-04-07 16:59:10 -0300

    ajuste corrigido com fiuncao no script js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-let nomePet = document.getElementById("petName").value;
-console.log(nomePet);
+document.querySelector("form").addEventListener("submit", function (e) {
+    e.preventDefault(); // impede o envio automático do form
+    let nomePet = document.getElementById("petName").value;
+    console.log("Nome do pet:", nomePet);
+  });
+  
