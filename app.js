0064b704189376f1128c4cc386c9ea423da47f6a
Fabricio <fabricioztxsantana@gmail.com>
2025-04-09 21:26:33 -0300

    ajustes de conexao backend  BD tabela

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 import  sqlite3 from 'sqlite3'//importa o banco 
+import express from 'express';
+import cors from 'cors';       // Importa o middleware CORS para permitir que o frontend acesse o backend
 import {open} from 'sqlite'// abre a conexão com banco de dados 
 
-async function criarEpopularTabelaUsuarios(nome,sobrenome){
-    const db = await open({          //só segue a execução se essa função der certo 
-        filename:'./Banco.db',        //caminho para o BD
-        driver: sqlite3.Database,   // executa BD
+const app = express();   //// Cria a aplicação Express
+const port = 3000;    //// Define a porta onde o servidor irá rodar
 
+app.use(express.json());  //// Permite que o servidor entenda requisições com JSON no corpo
+app.use(cors());  //// Permite que o frontend acesse esse backend mesmo se estiver em outra porta
+//-----------------------------------------------------------------------------BD----
+
+async function criarTabela() {
+    const db = await open({            // Abre conexão com o banco
+      filename: './Banco.db',
+      driver: sqlite3.Database
     });
-
-await db.run(`CREATE TABLE IF NOT EXISTS  Pets (id INTEGER PRIMARY KEY, petName TEXT, idade INTEGER, especie TEXT,nomeDono TEXT )`); //cria tabela caso el
-    
-    
-
-    
-
-}
-criarEpopularTabelaUsuarios();
+  
+    await db.exec(`                   // Executa o comando SQL
+      CREATE TABLE IF NOT EXISTS pets (
+        id INTEGER PRIMARY KEY, -- ID único
+        nomePet TEXT,                         -- Nome do pet
+        especie TEXT,                         -- Espécie
+        idade INTEGER,                        -- Idade
+        nomeDono TEXT                         -- Nome do dono
+      )
+    `);
+  }
+  
